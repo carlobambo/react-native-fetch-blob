9bb7be804fe9a0c13997c71de2ef227220f487cd
John Carlo Bambo <carlo@wizy.io>
2018-08-08 14:59:15 +0800

    Always use Fileprovider

--- android/src/main/java/com/RNFetchBlob/RNFetchBlob.java
+++ android/src/main/java/com/RNFetchBlob/RNFetchBlob.java
@@ -111,27 +111,19 @@ public class RNFetchBlob extends ReactContextBaseJavaModule {
         try {
             Uri uriForFile = FileProvider.getUriForFile(getCurrentActivity(),
                     this.getReactApplicationContext().getPackageName() + ".provider", new File(path));
+            
+            Intent intent = new Intent(Intent.ACTION_VIEW)
+                    .setDataAndType(uriForFile, mime);
 
-            if (Build.VERSION.SDK_INT >= 24) {
-                // Create the intent with data and type
-                Intent intent = new Intent(Intent.ACTION_VIEW)
-                        .setDataAndType(uriForFile, mime);
+            // Set flag to give temporary permission to external app to use FileProvider
+            intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_ACTIVITY_NEW_TASK);
 
-                // Set flag to give temporary permission to external app to use FileProvider
-                intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-
-                // Validate that the device can open the file
-                PackageManager pm = getCurrentActivity().getPackageManager();
-                if (intent.resolveActivity(pm) != null) {
-                    this.getReactApplicationContext().startActivity(intent);
-                }
-
-            } else {
-                Intent intent = new Intent(Intent.ACTION_VIEW)
-                        .setDataAndType(Uri.parse("file://" + path), mime).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-
+            // Validate that the device can open the file
+            PackageManager pm = getCurrentActivity().getPackageManager();
+            if (intent.resolveActivity(pm) != null) {
                 this.getReactApplicationContext().startActivity(intent);
             }
+
             ActionViewVisible = true;
 
             final LifecycleEventListener listener = new LifecycleEventListener() {
